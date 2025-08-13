8bb3666cec141b4e6bb062b24bb2f70623b45a37
nuoliainen <vvvahinko@gmail.com>
2025-08-14 00:44:28 +0300

    Edit and add new classes

--- init.sql
+++ init.sql
@@ -4,6 +4,19 @@ INSERT INTO classes (title, value) VALUES ('Vesi', 'makea vesi');
 INSERT INTO classes (title, value) VALUES ('Vesi', 'murtovesi');
 INSERT INTO classes (title, value) VALUES ('Vesi', 'merivesi');
 
+INSERT INTO classes (title, value) VALUES ('Lämpötila', 'trooppinen');
+INSERT INTO classes (title, value) VALUES ('Lämpötila', 'lauhkea');
+INSERT INTO classes (title, value) VALUES ('Lämpötila', 'kylmä');
+
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'vain kaloja');
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'vain selkärangattomia');
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'riutta-akvaario');
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'kasviakvaario');
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'biotooppi');
+INSERT INTO classes (title, value) VALUES ('Tyyppi', 'aquascaping');
+
 INSERT INTO classes (title, value) VALUES ('Tekniikka', 'low tech');
-INSERT INTO classes (title, value) VALUES ('Tekniikka', 'medium tech');
 INSERT INTO classes (title, value) VALUES ('Tekniikka', 'high tech');
+
+INSERT INTO classes (title, value) VALUES ('Sijainti', 'yksityinen');
+INSERT INTO classes (title, value) VALUES ('Sijainti', 'julkinen');
