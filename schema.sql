223e8e002a5129929e9861f14b212cba90ac4e5e
nuoliainen <vvvahinko@gmail.com>
2025-08-16 21:47:26 +0300

    Add indexes to improve query performance

--- schema.sql
+++ schema.sql
@@ -35,7 +35,6 @@ CREATE TABLE images (
     image BLOB
 );
 
-
 CREATE TABLE critters (
     id INTEGER PRIMARY KEY,
     user_id INTEGER REFERENCES users,
@@ -51,3 +50,16 @@ CREATE TABLE comments (
     content TEXT,
     sent_at TEXT
 );
+
+-- Indexes for aquariums
+CREATE INDEX idx_aquariums_user_id ON aquariums(user_id);
+
+-- Indexes for images
+CREATE INDEX idx_images_aquarium_id ON images(aquarium_id);
+
+-- Indexes for critters
+CREATE INDEX idx_critters_user_id ON critters(user_id);
+CREATE INDEX idx_critters_aquarium_id ON critters(aquarium_id);
+
+-- Indexes for comments
+CREATE INDEX idx_comments_aquarium_id ON comments(aquarium_id);
